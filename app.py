e08200ddb469070827b8cebb51274a4c8f5a3b64
Toomore Chiang <toomore0929@gmail.com>
2013-02-25 17:08:03 +0800

    新增 menu 選單

--- app.py
+++ app.py
@@ -8,19 +8,20 @@ from flask import url_for
 import t
 app = Flask(__name__)
 
-
-@app.route("/")
-def hello():
+def getmenu():
     return u"<a href='{0}'>登錄信</a> | <a href='{1}'>歡迎信</a>".format(
         url_for('sendfirst'), url_for('sendwelcome'))
 
+@app.route("/")
+def hello():
+    return getmenu()
 
 @app.route("/send_welcome", methods=['POST', 'GET'])
 def sendwelcome():
     if request.method == "POST":
         t.template = t.env.get_template('./coscup_welcome.htm')
         t.send_welcome(request.form.to_dict())
-        return u'{0}'.format(request.form.to_dict())
+        return u'{0}<br>{1}'.format(request.form.to_dict(), getmenu())
     else:
         return make_response(render_template('t_sendwelcome.htm'))
 
@@ -30,10 +31,10 @@ def sendfirst():
     if request.method == "POST":
         t.template = t.env.get_template('./coscup_first.htm')
         t.send_first(request.form.to_dict())
-        return u'{0}'.format(request.form.to_dict())
+        return u'{0}<br>{1}'.format(request.form.to_dict(), getmenu())
     else:
         return make_response(render_template('t_sendfirst.htm'))
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='127.0.0.1',debug=True)
