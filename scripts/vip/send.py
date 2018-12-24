447bb2ce8b1d3114b12acd58076a4a34c2b6c883
Toomore Chiang <toomore0929@gmail.com>
2018-12-24 10:36:33 +0000

    Add vip tpl in 2018

--- scripts/vip/send.py
+++ scripts/vip/send.py
@@ -24,14 +24,17 @@ def by_csv(csv_path, template_path):
 
         for row in reader:
             row = {key: unicode(value, 'utf8') for key, value in row.iteritems()}
+            if 'completed' in row and row['completed'] != '-':
+                continue
+
             row['body'] = template.render(row)
             print send(row)
 
 def send(info):
     print "sending <%(name)s, %(email)s>" % info
     return ses.send_email(
-        source='COSCUP2015 Attendee <attendee@coscup.org>',
-        subject=u'COSCUP2015 VIP program',
+        source='COSCUP2018 Attendee <attendee@coscup.org>',
+        subject=u'[Important Reminder 6/19 23:50(+8)] COSCUP2018 VIP program',
         to_addresses='"%s" <%s>' % (Header(info['name'], 'utf-8'), info['email']),
         format='html',
         return_path='attendee@coscup.org',
@@ -41,6 +44,9 @@ def send(info):
 
 if __name__ == '__main__':
     #by_csv('./vip_no.csv', './tpl_vip_no.htm')
+    #by_csv('./2018_no_vip.csv', './tpl_vip_no.htm')
     #by_csv('./vip_yes.csv', './tpl_vip_yes.htm')
+    by_csv('./coscup_vip_remind.csv', './tpl_vip_yes.htm')
+    #by_csv('./2018_list_2.csv', './tpl_vip_yes.htm')
     #by_csv('./vip_help.csv', './tpl_vip_help.htm')
     pass
