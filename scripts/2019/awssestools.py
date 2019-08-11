8799e4ce4932570b2b3819e3fe404d8c472c2a09
Toomore Chiang <toomore0929@gmail.com>
2019-08-11 11:09:29 +0000

    Add reminder_workers

--- scripts/2019/awssestools.py
+++ scripts/2019/awssestools.py
@@ -217,7 +217,7 @@ class AwsSESTools(object):
         #return msg_all.as_string()
 
     def send_attach_program(self, **kwargs):
-        ''' still in dev
+        ''' send attachment for program
 
         :param str source: from
         :param str to_addresses: to
@@ -254,6 +254,53 @@ class AwsSESTools(object):
 
         #image
         with open('./qrcode/program/%s.png' % kwargs['token'], 'rb') as i:
+            img = MIMEImage(i.read())
+
+        img.add_header('Content-Disposition', 'attachment; filename=%s.png' % kwargs['token'])
+        msg_all.attach(img)
+
+        return self.client.send_raw_email(
+                RawMessage={'Data': msg_all.as_string()})
+        #return msg_all.as_string()
+
+    def send_attach_worker(self, **kwargs):
+        ''' send attachment for worker
+
+        :param str source: from
+        :param str to_addresses: to
+        :param str subject: subject
+        :param str body: body
+        :param str token: token
+
+        '''
+        msg_all = MIMEMultipart()
+        msg_all['From'] = kwargs['source']
+        msg_all['To'] = kwargs['to_addresses']
+        msg_all['Subject'] = kwargs['subject']
+
+        msg_all.attach(MIMEText(kwargs['body'], 'html', 'utf-8'))
+
+        ics = render_ics(
+            title=u'COSCUP 2019',
+            description=u"No.43, Keelung Rd., Sec.4, Da'an Dist., Taipei 10607, Taiwan",
+            location=u'10607 臺北市大安區基隆路四段43號',
+            all_day=True,
+            start='20190817',
+            end='20190819',
+            created=datetime.now(),
+            admin=u'COSCUP2019 Attendee',
+            admin_mail=u'attendee@coscup.org',
+            url=u'https://coscup.org/2019/'
+        )
+        attachment = MIMEBase('text', 'calendar; name=calendar.ics; method=REQUEST; charset=UTF-8')
+        attachment.set_payload(ics.encode('utf-8'))
+        encoders.encode_base64(attachment)
+        attachment.add_header('Content-Disposition', 'attachment; filename=%s' % "calendar.ics")
+
+        msg_all.attach(attachment)
+
+        #image
+        with open('./qrcode/worker/%s.png' % kwargs['token'], 'rb') as i:
             img = MIMEImage(i.read())
 
         img.add_header('Content-Disposition', 'attachment; filename=%s.png' % kwargs['token'])
@@ -663,6 +710,31 @@ def reminder_program_speak(path, temp, title, dry_run=True):
                 print(AwsSESTools.mail_header(u['name'], u['mail']), u['token'])
 
 
+def reminder_workers(path, temp, title, dry_run=True):
+    template = TPLENV.get_template(temp)
+
+    with open(path, 'r+') as files:
+        csv_reader = csv.DictReader(files)
+        _n = 0
+        for u in csv_reader:
+            print('>>>', _n)
+            _n += 1
+            if not dry_run:
+                print(u['mail'])
+                print(AwsSESTools(setting.AWSID, setting.AWSKEY).send_attach_worker(
+                    source=AwsSESTools.mail_header(u'COSCUP 行政組', 'secretary@coscup.org'),
+                    to_addresses=AwsSESTools.mail_header(u['name'], u['mail']),
+                    subject=title,
+                    body=template.render(**u),
+                    token=u['token'],
+                ))
+                return
+            else:
+                if not Path('./qrcode/worker/%s.png' % u['token']).is_file():
+                    raise Exception('no png file', u['token'])
+                print(AwsSESTools.mail_header(u['name'], u['mail']), u['token'])
+
+
 if __name__ == '__main__':
     #worker_1('worker_1.csv', dry_run=False)
     #worker_2('works_form.csv', dry_run=False)
@@ -692,6 +764,12 @@ if __name__ == '__main__':
     #        path='./program_invite.csv',
     #        temp='./program_invite.html',
     #        title=u'[COSCUP2019] [Please Read!] Your ticket and information about COSCUP2019',
+    #        dry_run=False,
+    #    )
+    #reminder_workers(
+    #        path='./worker_token.csv',
+    #        temp='./worker_reminder.html',
+    #        title=u'COSCUP2019 工人行前通知信',
     #        dry_run=False,
     #    )
     pass
