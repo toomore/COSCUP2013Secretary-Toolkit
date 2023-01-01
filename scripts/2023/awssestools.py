fc4961c3c1c9853f6cb7ed20e74b82657a233c6e
Toomore Chiang <toomore0929@gmail.com>
2023-01-01 06:25:55 +0000

    Update random contents
    Signed-off-by: Toomore Chiang <toomore0929@gmail.com>

--- scripts/2023/awssestools.py
+++ scripts/2023/awssestools.py
@@ -13,7 +13,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.utils import formataddr
 from pathlib import Path
-from random import choice, sample, shuffle
+from random import choice, sample
 from uuid import uuid4
 
 import boto3
@@ -509,11 +509,11 @@ class AwsSESTools(object):
 
 def shuffle_list(data):
     ''' shuffle list '''
-    return shuffle(data)
+    return sample(data, len(data))
 
 
 def send_coscup_start(dry_run=True):
-    TPLENV.filters['shuffle'] = shuffle_list
+    TPLENV.filters['shuffle_list'] = shuffle_list
     template = TPLENV.get_template('./coscup_2023_inline.html')
     template_md = TPLENV.get_template('./coscup_2023.md')
 
